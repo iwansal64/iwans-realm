da447ec7ded249d5d0c63a9d065fcbb978bcb3d5
iwansal64 <iwansal64@gmail.com>
2024-12-05 19:18:55 +0000

    DONE BRANCHES (not include sub-branches)

---
+++ src/variables/categories.ts
@@ -0,0 +1,16 @@
+export const categories: {
+    name: string,
+    color: string
+}[] = [{
+    name: "Programming",
+    color: "#2512cf"
+},{
+    name: "Cyber Security",
+    color: "#721462"
+},{
+    name: "Electronics",
+    color: "#616724"
+},{
+    name: "Website",
+    color: "#0fc91a"
+}]
--- src/variables/projects.ts
+++ src/variables/projects.ts
@@ -75,20 +75,3 @@ export const projects_data: {
         state: "On Progress"
     },
 ];
-
-export const categories: {
-    name: string,
-    color: string
-}[] = [{
-    name: "Programming",
-    color: "#2512cf"
-},{
-    name: "Cyber Security",
-    color: "#721462"
-},{
-    name: "Electronics",
-    color: "#616724"
-},{
-    name: "Website",
-    color: "#0fc91a"
-}]
