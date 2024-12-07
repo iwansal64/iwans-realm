54b2c0038c6e358129a4af4ac6ec23c1c37dc2bb
Iwansal64 <iwansal64@gmail.com>
2024-12-07 13:57:20 +0700

    Achievements page is complete

---
+++ src/variables/achievements.ts
@@ -0,0 +1,18 @@
+export type Achievement = {
+    title: string,
+    description: string,
+    date_get: Date;
+};
+
+export const achievements: Achievement[] = [
+    {
+        title: "Best Subject in Mathematics 2023/2024",
+        description: "I've got this achievement because of being the best subject in mathematics. I've got this from Mitra Industri Vocational HighSchool",
+        date_get: new Date("2024-07-13T10:50:10")
+    },
+    {
+        title: "Best Subject in Electronics 2023/2024",
+        description: "I've got this achievement because of being the best subject in electronics. I've got this from Mitra Industri Vocational HighSchool",
+        date_get: new Date("2024-07-13T10:50:10")
+    }
+];
