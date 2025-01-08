27ec5dbeef40a0eaed70cea5db77ade8d461df41
Iwansal64 <iwansal64@gmail.com>
2025-01-08 20:48:34 +0700

    Updates some variables

--- src/variables/achievements.ts
+++ src/variables/achievements.ts
@@ -8,12 +8,12 @@ export type Achievement = {
 export const achievements: Achievement[] = [
     {
         title: "Best Subject in Mathematics 2023/2024",
-        description: "I've got this achievement because of being the best subject in mathematics. I've got this from Mitra Industri Vocational HighSchool",
+        description: "I've got this achievement because of being the best subject in mathematics. This achievements given by Mitra Industri Vocational HighSchool because becoming the best student at Mathematics",
         date_get: new Date("2024-07-13T10:50:10")
     },
     {
         title: "Best Subject in Electronics 2023/2024",
-        description: "I've got this achievement because of being the best subject in electronics. I've got this from Mitra Industri Vocational HighSchool",
+        description: "I've got this achievement because of being the best subject in electronics. This achievements given by Mitra Industri Vocational HighSchool because becoming the best student at Electronics",
         date_get: new Date("2024-07-13T10:50:10")
     },
     {
@@ -25,7 +25,7 @@ export const achievements: Achievement[] = [
     {
         title: "JavaScript Algorithm and Data Structures",
         description: "This certificate proofs that I can create an algorithm that can solves many problems with JavaScript",
-        date_get: new Date("2025-01-05T21:25:50"),
+        date_get: new Date("2024-03-07T21:25:50"),
         link: "https://www.freecodecamp.org/certification/Iwann/javascript-algorithms-and-data-structures-v8"
     }
 ];
--- src/variables/friends.ts
+++ src/variables/friends.ts
@@ -13,6 +13,11 @@ export const friends: Friend[] = [
         sub_skills: ["Science", "Mathematics"]
     },
     {
+        name: "Thoriq",
+        short_desc: "He's also one of my best friends from Mitra Industri Vocational HighSchool. He's so smart, he has a vast general knowledge. He also can speaks Germany and excel at Electricity!",
+        sub_skills: ["Electricity", "Germany"]
+    },
+    {
         name: "Aldo",
         short_desc: "He's my online friend. He's from Texas and also my first friend from outside country",
         sub_skills: ["Programming", "Cyber Security"]
