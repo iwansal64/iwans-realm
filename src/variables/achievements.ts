02c95a9a95d172afa2356f51ac0eb4fe5533b65a
Iwansal64 <iwansal64@gmail.com>
2025-01-08 20:07:52 +0700

    Solving Responsive Design Problems

--- src/variables/achievements.ts
+++ src/variables/achievements.ts
@@ -14,5 +14,10 @@ export const achievements: Achievement[] = [
         title: "Best Subject in Electronics 2023/2024",
         description: "I've got this achievement because of being the best subject in electronics. I've got this from Mitra Industri Vocational HighSchool",
         date_get: new Date("2024-07-13T10:50:10")
+    },
+    {
+        title: "Best Subject in Electronics 2023/2024",
+        description: "I've got this achievement because of being the best subject in electronics. I've got this from Mitra Industri Vocational HighSchool",
+        date_get: new Date("2024-07-13T10:50:10")
     }
 ];
